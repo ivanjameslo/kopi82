3d972a80ae4dae8ad79b2dcbad898459bb35ecc4
ellyannadu <duellyanna@gmail.com>
2024-08-22 22:32:29 +0800

    goodnight!

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,6 +1,6 @@
 import MenuRow from "@/components/MenuRow";
 import ProductsCard from "@/components/ProductsCard";
-import { picaPica } from "@/lib/menuitems";
+import { klassicKopi, picaPica } from "@/lib/menuitems";
 import React from "react";
 
 const MenuPage = () => {
@@ -12,11 +12,19 @@ const MenuPage = () => {
     />
   ));
 
+  const renderKlassicKopi = klassicKopi.map((link) => (
+    <ProductsCard
+      productName={link.productName}
+      productPrice={link.productPrice}
+      productImage={link.productImage}
+    />
+  ));
+
   return (
     <>
       <div className="mt-20">
         <MenuRow label="Pica Pica">{renderPicaPica}</MenuRow>
-        <MenuRow label="Klassic Kopi">{renderPicaPica}</MenuRow>
+        <MenuRow label="Klassic Kopi">{renderKlassicKopi}</MenuRow>
         <MenuRow label="Non Kopi">{renderPicaPica}</MenuRow>
       </div>
     </>
--- lib/menuitems.ts
+++ lib/menuitems.ts
@@ -1,3 +1,275 @@
+export const klassicKopi = [
+  {
+    productName: "Americano",
+    productPrice: "Hot: 95 | Iced: 105",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Latte",
+    productPrice: "Hot: 120 | Iced: 130",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Cappucino",
+    productPrice: "Hot: 120 | Iced: 130",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Mocha",
+    productPrice: "Hot: 140 | Iced: 170",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Salted Caramel",
+    productPrice: "Hot: 140 | Iced: 170",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "White Mocha Latte",
+    productPrice: "Hot: 140 | Iced: 170",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Love Latte",
+    productPrice: "Iced: 170",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const koldBrew = [
+  {
+    productName: "White Eye",
+    productPrice: "190",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Black Eye",
+    productPrice: "160",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Red Eye",
+    productPrice: "190",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const nonKopi = [
+  {
+    productName: "Matcha Latte",
+    productPrice: "Hot: 160 | Iced: 180",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Bellagio Dark Choco",
+    productPrice: "Hot: 130 | Iced: 160",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Cookies and Cream",
+    productPrice: "195",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Mango Cream",
+    productPrice: "195",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Strawberry Cream",
+    productPrice: "195",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const fusionTeas = [
+  {
+    productName: "Passion Teajito",
+    productPrice: "Hot: 110 | Iced: 120",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Mango Tango",
+    productPrice: "Hot: 110 | Iced: 120",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Menta Strawberry Lychee Tea",
+    productPrice: "Hot: 110 | Iced: 120",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Menta Mixed Berry Lychee Tea",
+    productPrice: "Hot: 110 | Iced: 120",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Frozen Lemonade",
+    productPrice: "160",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const allDayBreakfast = [
+  {
+    productName: "Longsilog",
+    productPrice: "200",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Sausilog",
+    productPrice: "Bistek Tagalog",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Chicken Steak with Mushroom Gravy",
+    productPrice: "200",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Beef Bulgogi",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const riceMeal = [
+  {
+    productName: "Chicken Ala King",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Chicken Parmigiana",
+    productPrice: "240",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Ginger Soy Shrimp Popcorn",
+    productPrice: "240",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Chili Cheese Bowl",
+    productPrice: "200",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Beef Steak with Mushroom Gravy",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Pork Steak with Mushroom Gravy",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Garlic Pepper Beef",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Lechon Kawali",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const pasta = [
+  {
+    productName: "Penne Chicken Alfredo",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Beef Meatball Marinara",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Seafood Marinara",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Spicy Shrimp Aglio Olio",
+    productPrice: "200",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Black Truffle Creamy Pasta",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Bacon and Mushroom Carbonara",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Creamy Bacon Mushroom Pasta",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Bacon Penne Telefono",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const pizza = [
+  {
+    productName: "All Cheese",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Garlic Shrimp",
+    productPrice: "230",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Cheesy Truffle",
+    productPrice: "250",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "All Meat",
+    productPrice: "230",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
+export const sandwich = [
+  {
+    productName: "Kani Salad Roll",
+    productPrice: "160",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Chicken Salad Roll",
+    productPrice: "160",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Chili Cheesedog",
+    productPrice: "230",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "French Toast BLT",
+    productPrice: "230",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Grilled Cheese",
+    productPrice: "220",
+    productImage: "/picapica/calamares.png",
+  },
+];
+
 export const picaPica = [
   {
     productName: "Calamares",
@@ -5,28 +277,53 @@ export const picaPica = [
     productImage: "/picapica/calamares.png",
   },
   {
-    productName: "Calamares",
+    productName: "Sausage Platter",
+    productPrice: "500",
+    productImage: "/picapica/calamares.png",
+  },
+  {
+    productName: "Pork Sisig",
     productPrice: "200",
     productImage: "/picapica/calamares.png",
   },
   {
-    productName: "Calamares",
-    productPrice: "200",
+    productName: "Nachos",
+    productPrice: "250",
     productImage: "/picapica/calamares.png",
   },
   {
-    productName: "Calamares",
-    productPrice: "200",
+    productName: "Mozzarella Sticks",
+    productPrice: "250",
     productImage: "/picapica/calamares.png",
   },
   {
-    productName: "Calamares",
-    productPrice: "200",
+    productName: "Fish & Chips",
+    productPrice: "220",
     productImage: "/picapica/calamares.png",
   },
   {
-    productName: "Calamares",
-    productPrice: "200",
+    productName: "Gambas",
+    productPrice: "220",
     productImage: "/picapica/calamares.png",
+  },
+];
+
+export const beer = [
+  {
+    productName: "Heineken",
+    productPrice: "75",
+    productImage: "/beers/heineken.png",
+  },
+  {
+    productName: "San Miguel Super Dry",
+    productPrice: "105",
+  },
+  {
+    productName: "San Miguel Premium",
+    productPrice: "110",
+  },
+  {
+    productName: "San Miguel Negra",
+    productPrice: "95",
   },
 ];
