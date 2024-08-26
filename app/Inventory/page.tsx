693c0021606fea3882f853f1185ab987d8058809
ellyannadu <duellyanna@gmail.com>
2024-08-26 20:23:02 +0800

    [Feat][Yunny] Added routes for Front and Back Inventory and changed the cards from white to black to look more sleek.

---
+++ app/Inventory/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InventoryRoutes from "@/components/InventoryRoutes";
+
+const page = () => {
+  return (
+    <div>
+      <InventoryRoutes />
+    </div>
+  );
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import PurchaseOrder from './PurchaseOrder/page'; // Adjust the path as necessary
-import AddPurchaseDetails from './Add-PurchaseDetails/page'; // Adjust the path as necessary
-import AddBackInventory from './Add-BackInventory/page'; // Adjust the path as necessary
-import AddFrontInventory from './Add-FrontInventory/page'; // Adjust the path as necessary
-import BackInventory from './BackInventory/page'; // Adjust the path as necessary
-import FrontInventory from './FrontInventory/page'; // Adjust the path as necessary
+import React from "react";
+import PurchaseOrder from "./PurchaseOrder/page"; // Adjust the path as necessary
+import AddPurchaseDetails from "./Add-PurchaseDetails/page"; // Adjust the path as necessary
+import AddBackInventory from "./Add-BackInventory/page"; // Adjust the path as necessary
+import AddFrontInventory from "./Add-FrontInventory/page"; // Adjust the path as necessary
+import BackInventory from "./BackInventory/page"; // Adjust the path as necessary
+import FrontInventory from "./FrontInventory/page"; // Adjust the path as necessary
 
 export default function Home() {
   return (
-    <main>
-      <BackInventory />
-    </main>
+    <main className="bg-[url('/background.png')] bg-cover bg-center min-h-screen"></main>
   );
 }
