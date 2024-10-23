a2005e5c2e1381d20d7c6a2b2b939247f2493884
ellyannadu <duellyanna@gmail.com>
2024-10-23 10:00:52 +0800

    sidebar refactor... again

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,28 +13,37 @@ export default function Sidebar() {
         <div
             onMouseEnter={() => setIsOpen(true)}
             onMouseLeave={() => setIsOpen(false)}
-            className={`flex flex-col min-h-screen relative transition-all duration-300 ${isOpen ? 'w-64' : 'w-16'
+            className={`flex flex-col min-h-screen relative transition-all duration-300 ${isOpen ? 'w-64' : 'w-28'
                 } bg-gradient-to-b from-[#FAEED1] to-[#B19470] p-4 shadow-lg`}
         >
             <div className="flex flex-col space-y-4">
                 <Link href="/" className="flex justify-center">
-                    <Image src="/kopi.png" alt="Kopi 82 Logo" width={40} height={40} />
+                    <Image
+                        src="/kopi.png"
+                        alt="Kopi 82 Logo"
+                        width={80}
+                        height={80}
+                        className="transition-all duration-300 h-[80px] w-[80px]"
+                    />
                 </Link>
-                {NAVBAR_LINKS.map((link) => (
-                    <Link
-                        key={link.label}
-                        href={link.href}
-                        className={`text-lg hover:underline flex items-center space-x-2 ${isOpen ? 'justify-start' : 'justify-center'
-                            }`}
-                        onClick={() => setIsOpen(false)}
-                    >
-                        {link.label === "Home" && <Home className="h-5 w-5" />}
-                        {link.label === "Menu" && <ShoppingCart className="h-5 w-5" />}
-                        {link.label === "Inventory" && <Box className="h-5 w-5" />}
-                        {link.label === "Sales" && <Wallet className="h-5 w-5" />}
-                        {isOpen && <span>{link.label}</span>}
-                    </Link>
-                ))}
+
+                <div className="pt-8 mt-8 flex flex-col space-y-8">
+                    {NAVBAR_LINKS.map((link) => (
+                        <Link
+                            key={link.label}
+                            href={link.href}
+                            className={`text-xl hover:underline flex items-center space-x-2 ${isOpen ? 'justify-start' : 'justify-center'
+                                }`}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            {link.label === "Home" && <Home className="h-7 w-7" />}
+                            {link.label === "Menu" && <ShoppingCart className="h-7 w-7" />}
+                            {link.label === "Inventory" && <Box className="h-7 w-7" />}
+                            {link.label === "Sales" && <Wallet className="h-7 w-7" />}
+                            {isOpen && <span>{link.label}</span>}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
