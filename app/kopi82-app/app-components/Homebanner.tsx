6ffbf9863830d6176abbe2d6ad5b198549688594
Ivan James Lo <ijalo@addu.edu.ph>
2024-12-01 23:53:30 +0800

    homebanner

--- app/kopi82-app/app-components/Homebanner.tsx
+++ app/kopi82-app/app-components/Homebanner.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image';
 
 const HomeBanner = () => {
     return ( 
-        <div className="relative bg-gradient-to-r
-        from-sky-500 to-sky-700 mb:8">
+        <div className="relative bg-gradient-to-r mb:8" style={{ background: 'linear-gradient(to right, #78562c, #78562c)' }}>
             <div className="mx-auto px-8 py-12 flex 
             flex-col gap-2 md:flex-row item-center
             justify-evenly">
                 <div className="mb-8 md:mb-0 text-center">
                     <h1 className="text-4xl md:text-6xl
-                    font-bold tex-white mb-4">Summer Sale</h1>
+                    font-bold text-white mb-4">Summer Sale</h1>
                     <p className="text-lg md:text-xl 
                     text-white mb-2">Enjoy discounts on selected Menus</p>
                     <p className="text-2xl md:text-5xl
